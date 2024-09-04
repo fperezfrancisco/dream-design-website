2ca3387c63806765a33aac39a292ad4e9c583adf
Francisco Perez <fperezfrancisco4@gmail.com>
2024-09-04 15:34:27 -0700

    added animation

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ menuOpenBtn.addEventListener("click", (e) => {
 menuCloseBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("open");
 });
+
+/** intersection observer */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+        return;
+      }
+      entry.target.classList.remove("in-view");
+    });
+  });
+
+  const allAnimatedElements = document.querySelectorAll(".animate");
+
+  allAnimatedElements.forEach((element) => observer.observe(element));
+});
